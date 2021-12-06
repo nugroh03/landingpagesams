c2ff22d9fda5a3332a30877c074017975591b110
nugroh03 <nugroho.dwi.cahyono@gmail.com>
2021-12-06 11:16:03 +0700

    noscript

--- pages/_app.js
+++ pages/_app.js
@@ -14,10 +14,10 @@ function MyApp({ Component, pageProps }) {
           rel='stylesheet'
         />
       </Head>
-      <Script
+      {/* <Script
         src='https://code.jquery.com/jquery-3.6.0.min.js'
         integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4='
-      ></Script>
+      ></Script> */}
       <Component {...pageProps} />
     </>
   );
