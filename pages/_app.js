7d0f31a1fc6c3543b7e870eddf5f49ab356646ab
nugroh03 <nugroho.dwi.cahyono@gmail.com>
2021-12-06 17:45:38 +0700

    new16

--- pages/_app.js
+++ pages/_app.js
@@ -8,17 +8,16 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com'/>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
           href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;800&display=swap'
           rel='stylesheet'
         />
         <script
-          id='header'
           src='https://code.jquery.com/jquery-3.6.0.min.js'
           integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4='
-          
-        />
+          crossorigin='anonymous'
+        ></script>
       </Head>
 
       <Component {...pageProps} />
