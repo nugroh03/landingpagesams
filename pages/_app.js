d38569060ca50694a8907fe92f85b811a8c4097d
nugroh03 <nugroho.dwi.cahyono@gmail.com>
2021-12-06 17:47:42 +0700

    new17

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
         <script
           src='https://code.jquery.com/jquery-3.6.0.min.js'
           integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4='
-          crossorigin='anonymous'
+          crossOrigin='anonymous'
         ></script>
       </Head>
 
